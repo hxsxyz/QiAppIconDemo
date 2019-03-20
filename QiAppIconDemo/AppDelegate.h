9cb97571e44d01d023364af582fdd29950190218
huangxianshuai <huangxianshuai@360.cn>
2019-03-20 18:01:27 +0800

    first commit

---
+++ QiAppIconDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QiAppIconDemo
+//
+//  Created by huangxianshuai on 2019/3/20.
+//  Copyright © 2019年 QiShare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QiAppIconDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  QiAppIconDemo
+//
+//  Created by huangxianshuai on 2019/3/20.
+//  Copyright © 2019年 QiShare. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
